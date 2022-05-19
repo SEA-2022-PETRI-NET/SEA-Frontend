0332d2cd025cd0a129723bfc6165fe7d6aee1264
marcusTm <marcusmathiesen@yahoo.dk>
2022-05-19 14:22:48 +0200

    build structure for frontend

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,5 +1,4 @@
 import Dashboard from '../components/Dashboard'
-import logo from './../logo.svg'
 import './App.css'
 
 function App() {
---
+++ frontend/src/components/PetriNetModelling.tsx
@@ -0,0 +1,11 @@
+import Box from '@mui/material/Box'
+
+export default function PetriNetModelling() {
+    return (
+        <>
+            <Box sx={{ display: 'flex' }}>
+                <h1> Modelling </h1>
+            </Box>
+        </>
+    )
+}
