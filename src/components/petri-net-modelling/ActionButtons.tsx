911baf29809d582b76214b2630a35aba050200d4
Lukas Kapp-Schwoerer <lukaska@student.ethz.ch>
2022-06-09 15:00:31 +0200

    add upload modal

--- src/components/petri-net-modelling/ActionButtons.tsx
+++ src/components/petri-net-modelling/ActionButtons.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react'
 import IconButton from '@mui/material/IconButton'
-import { Tooltip } from '@mui/material'
+import { Dialog, DialogTitle, List, ListItem, ListItemAvatar, Avatar, Tooltip } from '@mui/material'
 import PlayArrowTwoToneIcon from '@mui/icons-material/PlayArrowTwoTone'
 import DeleteIcon from '@mui/icons-material/Delete'
 import SaveIcon from '@mui/icons-material/Save'
+import CloseIcon from '@mui/icons-material/Close'
 import CheckCircleTwoToneIcon from '@mui/icons-material/CheckCircleTwoTone'
 import FileDownloadTwoToneIcon from '@mui/icons-material/FileDownloadTwoTone'
 import UploadTwoToneIcon from '@mui/icons-material/UploadTwoTone'
@@ -10,6 +12,9 @@ import { getPetriNetById, savePetriNet } from '../../api/petri-net-modelling'
 import { toast } from 'react-toastify'
 import { PetriNet, Place, Transition, Arc } from '../../models/PetrinetModels'
 import ReactFlow, { Node, Edge } from 'react-flow-renderer'
+import { InboxOutlined } from '@ant-design/icons'
+import type { UploadProps } from 'antd'
+import { message, Upload } from 'antd'
 
 interface ActionButtons {
     style?: React.CSSProperties | undefined
@@ -20,6 +25,75 @@ interface ActionButtons {
     setSelectedNode: any
 }
 
+const { Dragger } = Upload
+
+const props: UploadProps = {
+    name: 'file',
+    multiple: true,
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    onChange(info) {
+        const { status } = info.file
+        if (status !== 'uploading') {
+            console.log(info.file, info.fileList)
+        }
+        if (status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully.`)
+        } else if (status === 'error') {
+            message.error(`${info.file.name} file upload failed.`)
+        }
+    },
+    onDrop(e) {
+        console.log('Dropped files', e.dataTransfer.files)
+    },
+}
+
+export interface UploadDialogProps {
+    open: boolean
+    onClose: () => void
+}
+
+function UploadDialog(props: UploadDialogProps) {
+    const { onClose, open } = props
+
+    const handleClose = () => {
+        console.log('closing upload modal')
+    }
+
+    return (
+        <Dialog PaperProps={{ sx: { width: '50%' } }} onClose={handleClose} open={open}>
+            <DialogTitle>
+                Upload file
+                {onClose ? (
+                    <IconButton
+                        aria-label="close"
+                        onClick={onClose}
+                        sx={{
+                            position: 'absolute',
+                            right: 8,
+                            top: 8,
+                            color: (theme) => theme.palette.grey[500],
+                        }}
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                ) : null}
+            </DialogTitle>
+            <div style={{ margin: '30px 80px 50px 80px' }}>
+                <Dragger {...props}>
+                    <p className="ant-upload-drag-icon">
+                        <InboxOutlined />
+                    </p>
+                    <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                    <p className="ant-upload-hint">
+                        Support for a single or bulk upload. Strictly prohibit from uploading
+                        company data or other band files
+                    </p>
+                </Dragger>
+            </div>
+        </Dialog>
+    )
+}
+
 export default function ActionButtons({
     style,
     nodes,
@@ -28,6 +102,16 @@ export default function ActionButtons({
     setEdges,
     setSelectedNode,
 }: ActionButtons) {
+    const [ulpoadModalOpen, setUploadModalOpen] = useState(false)
+
+    const handleClickUpload = () => {
+        setUploadModalOpen(true)
+    }
+
+    const handleCloseUploadModal = () => {
+        setUploadModalOpen(false)
+    }
+
     const getCurrentPetriNet = () => {
         const petriNet = {} as PetriNet
         petriNet.id = 0
@@ -128,10 +212,11 @@ export default function ActionButtons({
                 </IconButton>
             </Tooltip>
             <Tooltip title="Upload">
-                <IconButton sx={{ margin: '0px 5px 0px 5px' }}>
+                <IconButton sx={{ margin: '0px 5px 0px 5px' }} onClick={handleClickUpload}>
                     <UploadTwoToneIcon sx={{ color: 'blue' }} />
                 </IconButton>
             </Tooltip>
+            <UploadDialog open={ulpoadModalOpen} onClose={handleCloseUploadModal} />
             <Tooltip title="Download">
                 <IconButton sx={{ margin: '0px 5px 0px 5px' }}>
                     <FileDownloadTwoToneIcon sx={{ color: 'blue' }} />
