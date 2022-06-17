310bd8711197a43d2da36407ac1d64f7d0299acb
marcusTm <marcusmathiesen@yahoo.dk>
2022-06-17 20:27:55 +0200

    upload works

--- src/components/petri-net-modelling/modals/UploadPetriNetDialog.tsx
+++ src/components/petri-net-modelling/modals/UploadPetriNetDialog.tsx
@@ -1,46 +1,35 @@
 import IconButton from '@mui/material/IconButton'
-import { Dialog, DialogTitle } from '@mui/material'
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
 import { InboxOutlined } from '@ant-design/icons'
-import type { UploadProps } from 'antd'
 import { message, Upload } from 'antd'
+import { useState } from 'react'
+import { PetriNet } from '../../../models/PetrinetModels'
 
 const { Dragger } = Upload
-
-const props: UploadProps = {
-    name: 'file',
-    multiple: true,
-    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-    onChange(info) {
-        const { status } = info.file
-        if (status !== 'uploading') {
-            console.log(info.file, info.fileList)
-        }
-        if (status === 'done') {
-            message.success(`${info.file.name} file uploaded successfully.`)
-        } else if (status === 'error') {
-            message.error(`${info.file.name} file upload failed.`)
-        }
-    },
-    onDrop(e) {
-        console.log('Dropped files', e.dataTransfer.files)
-    },
-}
 
 export interface UploadDialogProps {
     open: boolean
     onClose: () => void
+    loadPetriNet: (petriNet: PetriNet) => void
 }
 
-export default function UploadPetriNetDialog(props: UploadDialogProps) {
-    const { onClose, open } = props
+export default function UploadPetriNetDialog({ open, onClose, loadPetriNet }: UploadDialogProps) {
+    const [file, setFile] = useState<Blob | null>(null)
 
-    const handleClose = () => {
-        console.log('closing upload modal')
+    const uploadJson = () => {
+        if (file) {
+            const reader = new FileReader()
+            reader.onload = (e) => {
+                const petriNet: PetriNet = JSON.parse(e.target?.result?.toString() ?? '')
+                loadPetriNet(petriNet)
+            }
+            reader.readAsText(file)
+        }
     }
 
     return (
-        <Dialog PaperProps={{ sx: { width: '50%' } }} onClose={handleClose} open={open}>
+        <Dialog PaperProps={{ sx: { width: '50%' } }} onClose={onClose} open={open}>
             <DialogTitle>
                 Upload file
                 <IconButton
@@ -56,18 +45,45 @@ export default function UploadPetriNetDialog(props: UploadDialogProps) {
                     <CloseIcon />
                 </IconButton>
             </DialogTitle>
-            <div style={{ margin: '30px 80px 50px 80px' }}>
-                <Dragger {...props}>
-                    <p className="ant-upload-drag-icon">
-                        <InboxOutlined />
-                    </p>
-                    <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                    <p className="ant-upload-hint">
-                        Support for a single or bulk upload. Strictly prohibit from uploading
-                        company data or other band files
-                    </p>
-                </Dragger>
-            </div>
+            <DialogContent>
+                <div style={{ margin: '30px 80px 50px 80px' }}>
+                    <Dragger
+                        name={'file'}
+                        accept={'application/json'}
+                        beforeUpload={(file) => {
+                            setFile(file)
+                            return false
+                        }}
+                        onChange={(info) => {
+                            const { status } = info.file
+                            if (status === 'done') {
+                                message.success(`${info.file.name} file uploaded successfully.`)
+                            } else if (status === 'error') {
+                                message.error(`${info.file.name} file upload failed.`)
+                            }
+                        }}
+                        action={(file) => {
+                            console.log(file)
+                            return 'done'
+                        }}
+                        onRemove={() => {
+                            setFile(null)
+                        }}
+                    >
+                        <p className="ant-upload-drag-icon">
+                            <InboxOutlined />
+                        </p>
+                        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                        <p className="ant-upload-hint">
+                            Support for a single or bulk upload. Strictly prohibit from uploading
+                            company data or other band files
+                        </p>
+                    </Dragger>
+                </div>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={uploadJson}>Upload</Button>
+            </DialogActions>
         </Dialog>
     )
 }
